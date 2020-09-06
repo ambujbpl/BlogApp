b02e03414e999eba34a4c4ee6f02e2cbac06b989
ambujdubey <ambujdubey89@gmail.com>
2020-09-06 19:48:20 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+
+// express app
+const app = express();
+const config = require('./config/config');
+console.log('config : ',config);
+const port = 3355;
+mongoose.connect(config.connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => {
+    app.listen(config.port);
+    console.log(`Node MongoBlog Application running on ${config.port}`);
+  })
+  .catch(err => console.log(err));
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
+// routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+// blog routes
+app.use('/blogs', blogRoutes);
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
