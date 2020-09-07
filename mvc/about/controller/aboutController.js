935d413dfcd2a15e9cece824832003702e9ebe98
ambujdubey <ambujdubey89@gmail.com>
2020-09-07 10:25:16 +0530

    update edit about functionality

--- mvc/about/controller/aboutController.js
+++ mvc/about/controller/aboutController.js
@@ -1,9 +1,39 @@
-// const About = require('../model/about');
+const About = require('../model/about');
 
 const about = (req, res) => {
-  res.render('about', { title: 'About' });
+  About.find().sort({ createdAt: -1 })
+    .then(result => {
+      res.render('about', { about: result, title: 'About' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+const about_details = (req, res) => {
+  const id = req.params.id;
+  About.findById(id)
+    .then(result => {
+      res.render('editAbout', { about: result, title: 'About edit' });
+    })
+    .catch(err => {
+      console.log(err);
+      res.render('404', { title: 'About not found' });
+    });
+}
+
+const about_update = (req, res) => {
+  About.updateOne({}, { body: req.body.body })
+    .then(result => {
+      res.redirect('/about');
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 module.exports = {
-  about
+  about,
+  about_details,
+  about_update
 }
--- mvc/about/route/aboutRoutes.js
+++ mvc/about/route/aboutRoutes.js
@@ -4,5 +4,7 @@ const aboutController = require('./../controller/aboutController');
 const router = express.Router();
 
 router.get('/', aboutController.about);
+router.get('/:id', aboutController.about_details);
+router.post('/updateAbout', aboutController.about_update);
 
 module.exports = router;
