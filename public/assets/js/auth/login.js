4213f2e1bed0d3975eac2bbbc7891dad7dd05c3c
ambujdubey <ambujdubey89@gmail.com>
2020-09-24 09:49:24 +0530

    update auth module UI

--- public/assets/js/auth/login.js
+++ public/assets/js/auth/login.js
@@ -1,40 +1,38 @@
 $( document ).ready(function() {
-  const form = document.querySelector('form');
-  const emailError = document.querySelector('.email.error');
-  const passwordError = document.querySelector('.password.error');
-  form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    $('#loader-page').show();
-    // reset errors
-    emailError.textContent = '';
-    passwordError.textContent = '';
-
-    // get values
-    const email = form.email.value;
-    const password = form.password.value;
-
-    try {
-      const res = await fetch('/login', { 
-        method: 'POST', 
-        body: JSON.stringify({ email, password }),
-        headers: {'Content-Type': 'application/json'}
-      });
-      const data = await res.json();
-      if (data.errors) {
-        emailError.textContent = data.errors.email;
-        passwordError.textContent = data.errors.password;
-      }
-      if (data.user) {
-        alertify.success(data.message);
-        setTimeout(()=> {
-          location.assign('/');
-        });
-      }
-    }
-    catch (err) {
-      console.log(err);
-      $('#loader-page').hide();
-    }
-  });
+  M.updateTextFields();
   $('#loader-page').hide();
 });
+
+loginFunction = async () => {
+  $('#loader-page').show();
+  const email = $('#email').val();
+  const password = $('#password').val();
+  const filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  if(email == '' || password == '' || !filter.test(email)) {
+    let err = ``;
+    if(email == '') err += `Email is required `;
+    else if(password == '') err += ` Password are required `;
+    else if (!filter.test(email)) err += ` It should be valid email id`;
+    else err += 'Email and Password are required';
+    alertify.success(err);
+    $('#loader-page').hide();
+  } else {
+    const res = await fetch('/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: {'Content-Type': 'application/json'}
+    });
+    const data = await res.json();
+    if (data.errors) {
+      let err = data.errors.email + ' ' + data.errors.password;
+      alertify.success(err);
+      $('#loader-page').hide();
+    }
+    if (data.user) {
+      alertify.success(data.message);
+      setTimeout(()=> {
+        location.assign('/');
+      },200);
+    }
+  }
+}
